c6c1f109514813c83a8d2e18cd1ae4b04350bf46
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 <toke.reines@gmail.com>
2021-09-07 14:56:00 +0200

    First part (1/4) of first week exercise

--- Advanced Programming/exercise-haskell-intro/app/Main.hs
+++ Advanced Programming/exercise-haskell-intro/app/Main.hs
@@ -1,3 +1,4 @@
+-- Task 1
 type Pos = (Int, Int)
 data Direction = North | South | East | West
 
@@ -10,3 +11,33 @@ move South  (x,y) = (x, y-1)
 moves :: [Direction] -> Pos -> Pos 
 moves [] (x, y) = (x, y)
 moves (d:ds) (x, y) = moves ds (move d (x, y))
+
+-- Task 2
+data Nat = Zero | Succ Nat
+    deriving (Eq, Show, Read, Ord) 
+
+natAdd :: Nat -> Nat -> Nat
+natAdd Zero Zero = Zero
+natAdd (Succ x) Zero = Succ (natAdd x Zero)
+natAdd Zero (Succ y) = Succ (natAdd Zero y)
+natAdd (Succ x) (Succ y) = Succ(Succ(natAdd x y))
+
+nat2Int :: Nat -> Integer
+nat2Int Zero = 0
+nat2Int (Succ x) = 1 + nat2Int x
+
+int2Nat :: Integer -> Nat
+int2Nat 0 = Zero
+int2Nat x = Succ (int2Nat (x - 1))
+
+-- Task 3
+-- All leaves in left/right subtree are less/greater than node value
+data Tree a = Leaf | Node a (Tree a) (Tree a)
+    deriving (Eq, Show, Read, Ord)
+
+insert :: Tree Int -> Int -> Tree Int
+insert Leaf x = Node x Leaf Leaf
+insert (Node n left right) x  
+    | x > n = Node n left (insert right x)
+    | x == n = Node n left right
+    | otherwise = Node n (insert left x) right
