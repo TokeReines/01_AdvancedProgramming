8e388954f416f05146e4519876a1ec072e545a60
Julian Wulff <julianwulff@gmail.com>
2021-09-14 15:21:59 +0200

    Halfway finshed part 2

--- Assignments/01/code/part2/src/Arithmetic.hs
+++ Assignments/01/code/part2/src/Arithmetic.hs
@@ -13,15 +13,14 @@ module Arithmetic
   )
 
 where
-import Debug.Trace
+
 import Definitions
 import Data.Either (fromRight)
 
 showExp :: Exp -> String
 -- |Simple 2 + -1 -> Add (Cst 2) (Sub (Cst 0) (Cst 2)))
-showExp (Cst x)   = show x
+showExp (Cst x)   = "(" ++ show x ++ ")"
 showExp (Add x y) = "(" ++ showExp x ++ "+" ++ showExp y ++ ")"
-showExp (Sub (Cst 0) y) = "(-" ++ showExp y ++ ")"
 showExp (Sub x y) = "(" ++ showExp x ++ "-" ++ showExp y ++ ")"
 showExp (Mul x y) = "(" ++ showExp x ++ "*" ++ showExp y ++ ")"
 showExp (Div x y) = "(" ++ showExp x ++ "`div`" ++ showExp y ++ ")"
@@ -40,13 +39,14 @@ evalSimple (Mul x y) = evalSimple x * evalSimple y
 evalSimple (Div x y) 
   | evalSimple y == 0 = error "Division by zero not allowed"
   | otherwise = evalSimple x `div` evalSimple y
-evalSimple (Pow x y) 
-  | evalSimple y == 0 = 1
+evalSimple (Pow x y)
+  | evalSimple y == 0 = evalSimple x * 0 + 1
   | otherwise = evalSimple x ^ evalSimple y
 evalSimple _ = error "Unmatched exp in evalSimple"
 
 extendEnv :: VName -> Integer -> Env -> Env
-extendEnv v n r = \v' -> if v' == v then Just n else r v
+extendEnv v n r = \v' -> if v' == v then Just n else r v'
+
 
 -- For debugging: evalFull x _  | trace ("evalFull: " ++ show x) False = undefined
 evalFull :: Exp -> Env -> Integer
@@ -56,8 +56,8 @@ evalFull (Var v) e = case e v of
     Nothing -> error "Variable not declared"
 evalFull Let{var=v, def=d, body=b} e = evalFull b (extendEnv v (evalFull d e) e)
 evalFull (Sum{var=v, from=f, to=t, body=b}) e
-    | from < to = (evalFull b extE) + (evalFull Sum{var=v, from=incF, to=t, body=b} extE)
-    | from == to = evalFull b extE
+    | from < to = (evalFull b extE) + (evalFull Sum{var=v, from=incF, to=t, body=b} e)
+    | from == to = (evalFull b extE)
     | otherwise = evalFull (Cst 0) extE
     where from = evalFull f e
           to = evalFull t e
@@ -66,50 +66,55 @@ evalFull (Sum{var=v, from=f, to=t, body=b}) e
 evalFull (Add x y) e = evalFull x e + evalFull y e
 evalFull (Sub x y) e = evalFull x e - evalFull y e
 evalFull (Mul x y) e = evalFull x e * evalFull y e
-evalFull (Pow x y) e = evalFull x e ^ evalFull y e
 evalFull (Div x y) e 
     | evalFull y e == 0 = error "Division by zero not allowed"
     | otherwise = evalFull x e `div` evalFull y e
+evalFull (Pow x y) e
+  | evalFull y e == 0 = (evalFull x e) * 0 + 1 
+  | otherwise = evalFull x e ^ evalFull y e
 evalFull x _ = evalSimple x
 
--- fromEither :: Either ArithError Integer ->
--- https://stackoverflow.com/questions/156013/haskell-syntax-for-a-case-expression-in-a-do-block
 evalErr :: Exp -> Env -> Either ArithError Integer
--- evalErr If {test=t, yes=y, no=n} e = if evalErr t e /= 0 then evalErr y e else evalErr n e
--- evalErr (Var v) e = case e v of 
---     Just a -> Righta
---     Nothing -> Left (EBadVar v)
-evalErr (Cst x) e | trace ("Cst: " ++ show x) False = undefined
 evalErr (Cst x) _ = Right x
-evalErr (Add x y) e | trace ("Add: " ++ show x ++ " " ++ show y) False = undefined
-evalErr (Add x y) e
-  | (Left _) <- first = first
-  | (Left _) <- second = second
-  | (Right _) <- first
-  , (Right _) <- second 
-  = Right ((fromRight 1 first) + (fromRight 1 second))
-  where first  = evalErr x e
-        second = evalErr y e
-evalErr (Sub x y) e | trace ("Sub: " ++ show x ++ " " ++ show y) False = undefined
-evalErr (Sub x y) e
-  | (Left _) <- first = first
-  | (Left _) <- second = second
-  | (Right _) <- first
-  , (Right _) <- second 
-  = Right ((fromRight 1 first) - (fromRight 1 second))
-  where first  = evalErr x e
-        second = evalErr y e
-evalErr (Pow x y) e | trace ("Pow: " ++ show x ++ " " ++ show y) False = undefined
-evalErr (Pow x y) e 
-  | (Left _) <- first = first
-  | (Left _) <- second = second
-  | (fromRight 1 second) <= 0 = Left ENegPower
-  | (Right _) <- first
-  , (Right _) <- second 
-  = Right ((fromRight 1 first) ^ (fromRight 1 second))
-  where first  = evalErr x e
-        second = evalErr y e
-evalErr (Div x y) e | trace ("Div: " ++ show x ++ " " ++ show y) False = undefined
+evalErr (Add x y) e = do 
+  a <- evalErr x e
+  b <- evalErr y e
+  return (a+b)
+evalErr (Sub x y) e = do 
+  a <- evalErr x e
+  b <- evalErr y e
+  return (a-b)
+evalErr (Pow x y) e = do 
+  a <- evalErr x e
+  b <- evalErr y e
+  if b > 0 then return (a^b) else (Left ENegPower)
+  -- | (Left _) <- first = first
+  -- | (Left _) <- second = second
+  -- | (Right _) <- first
+  -- , (Right _) <- second 
+  -- = Right ((fromRight 1 first) + (fromRight 1 second))
+  -- where first  = evalErr x e
+  --       second = evalErr y e
+-- evalErr (Sub x y) e
+--   | (Left _) <- first = first
+--   | (Left _) <- second = second
+--   | (Right _) <- first
+--   , (Right _) <- second 
+--   = Right ((fromRight 1 first) - (fromRight 1 second))
+--   where first  = evalErr x e
+--         second = evalErr y e
+-- evalErr (Mul x y) e
+--   | (Left _) <- first = first
+--   | (Left _) <- second = second
+-- evalErr (Pow x y) e 
+--   | (Left _) <- first = first
+--   | (Left _) <- second = second
+--   | (fromRight 1 second) <= 0 = Left ENegPower
+--   | (Right _) <- first
+--   , (Right _) <- second 
+--   = Right ((fromRight 1 first) ^ (fromRight 1 second))
+--   where first  = evalErr x e
+--         second = evalErr y e
 evalErr (Div x y) e 
   | (Left _) <- first = first
   | (Left _) <- second = second
@@ -120,39 +125,9 @@ evalErr (Div x y) e
   where first  = evalErr x e
         second = evalErr y e
 
--- https://stackoverflow.com/a/46944385
+-- data Eithera b = Lefta| Rightb-- in standard prelude
+-- instance Monad (Eithere) wherereturn a= Right a(Left e)  >>=f= Lefte(Righta) >>=f=f a
 
--- evalErr (Div x y) e = do
---     first <- evalErr x e
---     second <- evalErr y e
---     return (case first of
---         (Right x) -> Right x
---         otherwise -> Left EDivZero)
-
--- evalErr (Add x y) e = evalErr x e
--- evalErr (Add x y) e 
---     | (Right _) <- x = Right first
---     | otherwise = Left ENegPower
---     where 
---         first = evalErr x e
---         second = evalErr y e
--- evalErr (Add x y) e 
---     | (Add Right x _ y) = evalErr  first
---     | otherwise = Left ENegPower
---     where 
---         first = evalErr x e
---         second = evalErr y e
--- evalErr x _ = Right (evalSimple x)
-
--- optional parts (if not attempted, leave them unmodified)
--- func = λx ->
-      -- let { k = bat x } in
-      -- let { j = baz k } in
-      -- +
-      --   (+ (bar j) j)
-      --   k
--- (Add (Div (Cst 2) (Cst 0)) (Pow (Cst 4) (Sub (Cst 0) (Cst 1)))) should show ((2*3)+4)
--- testX = evalErr(Add (Cst 2) (Cst 2)) initEnv -- should show ((2*3)+4) 
 showCompact :: Exp -> String
 showCompact = undefined
 
@@ -163,5 +138,12 @@ evalLazy :: Exp -> Env -> Either ArithError Integer
 evalLazy = undefined
 
 -- tests
---
+e = (extendEnv "x" 5 (extendEnv "y" 6 initEnv)) 
+-- sum x = 1 to 
+
+
+-- 1 +(2)? 
+s = evalFull (Sum "x" (Cst 1) (Var "x") (Let "x" (Add (Var "x") (Cst 1)) (Var "x"))) e
+l = (s == 20)
+-- sum = x = 1 -> x where x 
 
--- Assignments/01/code/part2/tests/Test.hs
+++ Assignments/01/code/part2/tests/Test.hs
@@ -6,13 +6,12 @@ import Arithmetic
 import Test.Tasty
 import Test.Tasty.HUnit
 
-
 main = defaultMain tests
 
 e = initEnv
 
 tests :: TestTree
-tests = testGroup "Tests" [showExpTests, evalSimpleTests, evalFullIfTests]
+tests = testGroup "Tests" [showExpTests, evalSimpleTests, evalFullIfTests, evallFullLetTests, evalFullSumTest]
 
 showExpTests = testGroup "showExp tests"
   [ testCase "showExp (Mul (Cst 2) (Add (Cst 3) (Cst 4))) should show (2*(3+4))" $ showExp (Mul (Cst 2) (Add (Cst 3) (Cst 4))) `compare` "(2*(3+4))" @?= EQ
@@ -33,11 +32,11 @@ evalSimpleTests = testGroup "evalSimple tests"
 evalFullIfTests = testGroup "evalFull If tests"
   [ testCase "If {test=Cst 1, yes=Cst 2, no=Cst 3} = 2" $ evalFull (If {test=Cst 1, yes=Cst 2, no=Cst 3}) e `compare` 2 @?= EQ
   , testCase "If {test=Cst 0, yes=Cst 2, no=Cst 3} = 3" $ evalFull (If {test=Cst 0, yes=Cst 2, no=Cst 3}) e `compare` 3 @?= EQ
-  , testCase "If {test = Sub (Cst 2) (Cst 2), yes = Div (Cst 3) (Cst 0), no = Cst 5} doesnt throw error" $ evalFull (If {test = Sub (Cst 2) (Cst 2), yes = Div (Cst 3) (Cst 0), no = Cst 5}) initEnv `compare` 2 @?= EQ
+  , testCase "If {test = Sub (Cst 2) (Cst 2), yes = Div (Cst 3) (Cst 0), no = Cst 5} doesnt throw error" $ evalFull (If {test = Sub (Cst 2) (Cst 2), yes = Div (Cst 3) (Cst 0), no = Cst 5}) initEnv `compare` 5 @?= EQ
   ]
 
 evallFullLetTests = testGroup "evalFull Let tests"
-  [ testCase "Let {var = "x", def = Add (Cst 3) (Cst 4),body = Mul (Var "x") (Var "x")} = 14" $ evalFull (Let {var = "x", def = Add (Cst 3) (Cst 4),body = Mul (Var "x") (Var "x")}) e `compare` 14 @?= EQ
+  [ testCase "Let {var = \"x\", def = Add (Cst 3) (Cst 4),body = Mul (Var \"x\") (Var \"x\")} = 14" $ evalFull (Let {var = "x", def = Add (Cst 3) (Cst 4),body = Mul (Var "x") (Var "x")}) e `compare` 14 @?= EQ
   , testCase "Update equal value here" $ evalFull (Let {var = "x", def = Cst 5, body = Add (Let {var = "x", def = Add (Cst 3) (Cst 4), body = Mul (Var "x") (Var "x")}) (Var "x")}) e `compare` 14 @?= EQ
   , testCase "Should not throw error" $ evalFull (Let "x"  (Div (Cst 4) (Cst 0))  (Cst 5)) e `compare` 14 @?= EQ
   ]
@@ -45,16 +44,3 @@ evallFullLetTests = testGroup "evalFull Let tests"
 evalFullSumTest = testGroup "evalFull Sum tests"
  [ testCase "Sum \"x\"  (Cst 1)  (Add (Cst 2) (Cst 2)) (Mul (Var \"x\") (Var \"x\")) = 30" $ evalFull (Sum "x"  (Cst 1)  (Add (Cst 2) (Cst 2)) (Mul (Var "x") (Var "x"))) e `compare` 30 @?= EQ
  ]
-
-
-
--- https://stackoverflow.com/questions/13350164/how-do-i-test-for-an-error-in-haskell
--- https://www.youtube.com/watch?v=PGsDvgmZF7A
--- readExamples :: IO [(Text, Text)]
--- readExamples = 
---   mapM asPair =<< Text.lines <$> Text.readFile "tests/showExp.csv"
---     where 
---       asPair line =
---         case Text.splitOn "," line of
---           [input, expected] -> pure (input, expected)
---           _ -> fail ("Invalid example line: " <> Text.unpack line)
