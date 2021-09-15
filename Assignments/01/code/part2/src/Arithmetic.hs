a1281e70d0d7e7799055b1e40603c0bdbfa9efa7
Julian Wulff <julianwulff@gmail.com>
2021-09-15 15:51:07 +0200

    Added a few comments, removed dead code in comments and parenthesis to constant are now only shown for negative integers

--- Assignments/01/code/part2/src/Arithmetic.hs
+++ Assignments/01/code/part2/src/Arithmetic.hs
@@ -1,5 +1,3 @@
--- This is a skeleton file for you to edit
-
 module Arithmetic
   (
   showExp,
@@ -15,11 +13,12 @@ module Arithmetic
 where
 
 import Definitions
-import Data.Either (fromRight)
 
 showExp :: Exp -> String
--- |Simple 2 + -1 -> Add (Cst 2) (Sub (Cst 0) (Cst 2)))
-showExp (Cst x)   = "(" ++ show x ++ ")"
+-- Simple
+showExp (Cst x)
+  | x < 0 = "(" ++ show x ++ ")" -- Add parenthesis to negative integers
+  | otherwise = show x
 showExp (Add x y) = "(" ++ showExp x ++ "+" ++ showExp y ++ ")"
 showExp (Sub x y) = "(" ++ showExp x ++ "-" ++ showExp y ++ ")"
 showExp (Mul x y) = "(" ++ showExp x ++ "*" ++ showExp y ++ ")"
@@ -48,21 +47,20 @@ extendEnv :: VName -> Integer -> Env -> Env
 extendEnv v n r = \v' -> if v' == v then Just n else r v'
 
 
--- For debugging: evalFull x _  | trace ("evalFull: " ++ show x) False = undefined
 evalFull :: Exp -> Env -> Integer
 evalFull If {test=t, yes=y, no=n} e = if evalFull t e /= 0 then evalFull y e else evalFull n e
 evalFull (Var v) e = case e v of 
     Just a -> a
     Nothing -> error "Variable not declared"
 evalFull Let{var=v, def=d, body=b} e = evalFull b (extendEnv v (evalFull d e) e)
-evalFull (Sum{var=v, from=f, to=t, body=b}) e
-    | from < to = (evalFull b extE) + (evalFull Sum{var=v, from=incF, to=t, body=b} e)
-    | from == to = (evalFull b extE)
+evalFull Sum{var=v, from=f, to=t, body=b} e
+    | from < to = evalFull b extE + evalFull Sum{var=v, from=incF, to=t, body=b} e
+    | from == to = evalFull b extE
     | otherwise = evalFull (Cst 0) extE
     where from = evalFull f e
           to = evalFull t e
           extE = extendEnv v from e
-          incF = Add f (Cst 1)
+          incF = Add f (Cst 1) -- Increments f for the recursive call of sum
 evalFull (Add x y) e = evalFull x e + evalFull y e
 evalFull (Sub x y) e = evalFull x e - evalFull y e
 evalFull (Mul x y) e = evalFull x e * evalFull y e
@@ -70,63 +68,56 @@ evalFull (Div x y) e
     | evalFull y e == 0 = error "Division by zero not allowed"
     | otherwise = evalFull x e `div` evalFull y e
 evalFull (Pow x y) e
-  | evalFull y e == 0 = (evalFull x e) * 0 + 1 
+  | evalFull y e == 0 = evalFull x e * 0 + 1 
   | otherwise = evalFull x e ^ evalFull y e
 evalFull x _ = evalSimple x
 
 evalErr :: Exp -> Env -> Either ArithError Integer
 evalErr (Cst x) _ = Right x
 evalErr (Add x y) e = do 
-  a <- evalErr x e
-  b <- evalErr y e
-  return (a+b)
+  x' <- evalErr x e
+  y' <- evalErr y e
+  return (x'+y')
 evalErr (Sub x y) e = do 
-  a <- evalErr x e
-  b <- evalErr y e
-  return (a-b)
+  x' <- evalErr x e
+  y' <- evalErr y e
+  return (x'-y')
+evalErr (Mul x y) e = do
+  x' <- evalErr x e
+  y' <- evalErr y e
+  return (x'*y')
+evalErr (Div x y) e = do
+  x' <- evalErr x e
+  y' <- evalErr y e
+  if y' /= 0 then return (x' `div` y') else Left EDivZero
 evalErr (Pow x y) e = do 
-  a <- evalErr x e
-  b <- evalErr y e
-  if b > 0 then return (a^b) else (Left ENegPower)
-  -- | (Left _) <- first = first
-  -- | (Left _) <- second = second
-  -- | (Right _) <- first
-  -- , (Right _) <- second 
-  -- = Right ((fromRight 1 first) + (fromRight 1 second))
-  -- where first  = evalErr x e
-  --       second = evalErr y e
--- evalErr (Sub x y) e
---   | (Left _) <- first = first
---   | (Left _) <- second = second
---   | (Right _) <- first
---   , (Right _) <- second 
---   = Right ((fromRight 1 first) - (fromRight 1 second))
---   where first  = evalErr x e
---         second = evalErr y e
--- evalErr (Mul x y) e
---   | (Left _) <- first = first
---   | (Left _) <- second = second
--- evalErr (Pow x y) e 
---   | (Left _) <- first = first
---   | (Left _) <- second = second
---   | (fromRight 1 second) <= 0 = Left ENegPower
---   | (Right _) <- first
---   , (Right _) <- second 
---   = Right ((fromRight 1 first) ^ (fromRight 1 second))
---   where first  = evalErr x e
---         second = evalErr y e
-evalErr (Div x y) e 
-  | (Left _) <- first = first
-  | (Left _) <- second = second
-  | (Right 0) <- second = Left EDivZero
-  | (Right _) <- first
-  , (Right _) <- second 
-  = Right ((fromRight 1 first) `div` (fromRight 1 second))
-  where first  = evalErr x e
-        second = evalErr y e
-
--- data Eithera b = Lefta| Rightb-- in standard prelude
--- instance Monad (Eithere) wherereturn a= Right a(Left e)  >>=f= Lefte(Righta) >>=f=f a
+  x' <- evalErr x e
+  y' <- evalErr y e
+  if y' >= 0 then return (x' ^ y') else Left ENegPower
+evalErr If {test=t, yes=y, no=n} e = do 
+  t' <- evalErr t e
+  if t' /= 0 then do
+    evalErr y e
+  else do
+    evalErr n e
+evalErr (Var x) e = 
+    case e x of 
+      Just a -> Right a
+      Nothing -> Left (EBadVar x)
+evalErr Let{var=v, def=d, body=b} e = do 
+  d' <- evalErr d e
+  evalErr b (extendEnv v d' e)
+evalErr Sum {var=v, from=f, to=t, body=b} e = do 
+  f' <- evalErr f e
+  t' <- evalErr t e
+  if f' < t' then do
+    b' <- evalErr b (extendEnv v f' e)
+    s' <- evalErr Sum{var=v, from=Add f (Cst 1), to=t, body=b} e
+    return (b' + s')
+  else if f' == t' then do
+    evalErr b (extendEnv v f' e)
+  else do
+    evalErr (Cst 0) (extendEnv v f' e)
 
 showCompact :: Exp -> String
 showCompact = undefined
@@ -136,14 +127,3 @@ evalEager = undefined
 
 evalLazy :: Exp -> Env -> Either ArithError Integer
 evalLazy = undefined
-
--- tests
-e = (extendEnv "x" 5 (extendEnv "y" 6 initEnv)) 
--- sum x = 1 to 
-
-
--- 1 +(2)? 
-s = evalFull (Sum "x" (Cst 1) (Var "x") (Let "x" (Add (Var "x") (Cst 1)) (Var "x"))) e
-l = (s == 20)
--- sum = x = 1 -> x where x 
-
