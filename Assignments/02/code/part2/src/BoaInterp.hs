4041a7b00e281d274d6d052e7aa89263d5d56ed7
Toke Reines <toke.reines@gmail.com>
2021-09-19 20:33:13 +0200

    Implemented more stuff

--- Assignments/02/code/part2/src/BoaInterp.hs
+++ Assignments/02/code/part2/src/BoaInterp.hs
@@ -47,9 +47,10 @@ look v = Comp (\e -> case lookup v e of
                       Just x -> (Right x, [])
                       Nothing -> (Left $ EBadVar v, [])
                       )
-
+-- Runs  the  computation m with x bound  to v,  
+-- in  addition  to  any  othercurrent bindings
 withBinding :: VName -> Value -> Comp a -> Comp a
-withBinding x v m = undefined -- let e' = v in m e
+withBinding x v m = Comp(\e -> runComp m (e ++ [(x, v)])) -- Might give duplicate variables in env
 
 output :: String -> Comp ()
 output s = Comp (\_e -> (Right (), [s]))
@@ -68,24 +69,50 @@ operate Plus (IntVal x) (IntVal y) = (Right . IntVal) (x + y)
 -- Right(IntVal(FuncA(FuncB(x + y))))
 -- operate Plus (StringVal x StringVal y) = Left x ++ y
 operate Plus _ _ = Left "Only integers allowed for addition"
-operate Minus x y= undefined
-operate Times x y =undefined
-operate Div x y = undefined
-operate Mod x y = undefined
-operate Eq x y = undefined
-operate Less x y = undefined
-operate Greater x y = undefined
-operate In x y = undefined
+operate Minus (IntVal x) (IntVal y) = (Right . IntVal) (x - y)
+operate Times (IntVal x) (IntVal y) = (Right . IntVal) (x * y)
+operate Div (IntVal x) (IntVal y) = (Right . IntVal) (x `div` y)
+operate Mod (IntVal x) (IntVal y) = (Right . IntVal) (x `mod` y)
+operate Eq (IntVal x) (IntVal y)
+  | x == y = Right TrueVal
+  | otherwise = Right FalseVal
+operate Less (IntVal x) (IntVal y)   
+  | x < y = Right TrueVal
+  | otherwise = Right FalseVal
+operate Greater (IntVal x) (IntVal y)    
+  | x > y = Right TrueVal
+  | otherwise = Right FalseVal
+-- Many more cases here. Haskell doesn't allow elem 1 ["hej"], but python does
+operate In (TrueVal) (ListVal l) 
+  | elem TrueVal l = Right TrueVal
+  | otherwise = Right FalseVal
 
 apply :: FName -> [Value] -> Comp Value
-apply = undefined
+apply f v 
+ --  | f == "print" = Comp (\e -> (Right v, [])) -- Stringify list of Value into monad output?
+  | f == "range" = case v of 
+    [IntVal x] -> let v = map (\x -> IntVal x) [0..1] 
+                  in Comp (\e -> (Right (ListVal v), []))
+    [IntVal x, IntVal y] -> let v = map (\x -> IntVal x) [x..y] 
+                            in Comp (\e -> (Right (ListVal v), []))
+    [IntVal x, IntVal y, IntVal z] -> let v = map (\x -> IntVal x) [z - 1, x..y]
+                                      in Comp (\e -> (Right (ListVal v), [])) -- [0, 1..3] -> [1,2,3]. z is 0-indexed]
+  | otherwise = undefined
 
 -- Main functions of interpreter
 -- eval e is the computation that evaluates the expression e in the current environment and returns its value
 eval :: Exp -> Comp Value
 eval (Const v) = Comp (\e -> (Right v, [])) -- Const List [Const Intval 1] or Const (IntVal 2)
--- eval Var v = undefined
--- eval Oper Op Exp Exp = undefined
+eval (Var v) = Comp (\e -> case lookup v e of {Just a -> (Right a, []); Nothing -> (Left (EBadVar v), [])})
+eval (Oper o x y) = do
+  x' <- eval x
+  y' <- eval y
+  v <- operate o x' y'
+  return $ case v of
+    Left e -> Comp (\e -> (Left e, []))
+    Right v -> Comp (\e -> (Right v, []))
+  
+
 -- eval Not Exp = undefined
 -- eval Call f [Exp] 
 -- eval Call f [] = undefined
