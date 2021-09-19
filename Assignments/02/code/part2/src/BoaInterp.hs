a001fc1c5bc020601cce303c342a5dadc93d62f3
Julian Wulff <julianwulff@gmail.com>
2021-09-19 23:29:30 +0200

    Updated the monad

--- Assignments/02/code/part2/src/BoaInterp.hs
+++ Assignments/02/code/part2/src/BoaInterp.hs
@@ -17,11 +17,16 @@ data RunError = EBadVar VName | EBadFun FName | EBadArg String
 
 newtype Comp a = Comp {runComp :: Env -> (Either RunError a, [String])}
 instance Monad Comp where
-  return a = Comp (\_e -> (Right a, []))
-  Comp m >>= f = Comp (\e -> 
-    case m e of
-      (Right a, out) -> runComp (f a) e
-      (Left re, out) -> (Left re, out))
+  return a = Comp (\e -> (Right a, []))
+  m >>= f = Comp (\e -> case runComp m e of
+                          (Left err, out) -> (Left err, out)
+                          (Right a, out) -> runComp (f a) e)
+  -- Comp m >>= f = Comp (\e -> 
+  --   case m e of
+  --     (Right a, out) -> runComp (f a) e
+  --     (Left re, out) -> (Left re, out))
+
+  
       
   -- Comp m >>= f = Comp (\e -> let (a, o) = runComp m e
   --                                (a', o') = runComp (f a) e
@@ -68,49 +73,52 @@ operate :: Op -> Value -> Value -> Either String Value
 operate Plus (IntVal x) (IntVal y) = (Right . IntVal) (x + y)
 -- Right(IntVal(FuncA(FuncB(x + y))))
 -- operate Plus (StringVal x StringVal y) = Left x ++ y
-operate Plus _ _ = Left "Only integers allowed for addition"
-operate Minus (IntVal x) (IntVal y) = (Right . IntVal) (x - y)
-operate Times (IntVal x) (IntVal y) = (Right . IntVal) (x * y)
-operate Div (IntVal x) (IntVal y) = (Right . IntVal) (x `div` y)
-operate Mod (IntVal x) (IntVal y) = (Right . IntVal) (x `mod` y)
-operate Eq (IntVal x) (IntVal y)
-  | x == y = Right TrueVal
-  | otherwise = Right FalseVal
-operate Less (IntVal x) (IntVal y)   
-  | x < y = Right TrueVal
-  | otherwise = Right FalseVal
-operate Greater (IntVal x) (IntVal y)    
-  | x > y = Right TrueVal
-  | otherwise = Right FalseVal
--- Many more cases here. Haskell doesn't allow elem 1 ["hej"], but python does
-operate In (TrueVal) (ListVal l) 
-  | elem TrueVal l = Right TrueVal
-  | otherwise = Right FalseVal
+operate _ _ _ = undefined
+-- operate Plus _ _ = Left "Only integers allowed for addition"
+-- operate Minus (IntVal x) (IntVal y) = (Right . IntVal) (x - y)
+-- operate Times (IntVal x) (IntVal y) = (Right . IntVal) (x * y)
+-- operate Div (IntVal x) (IntVal y) = (Right . IntVal) (x `div` y)
+-- operate Mod (IntVal x) (IntVal y) = (Right . IntVal) (x `mod` y)
+-- operate Eq (IntVal x) (IntVal y)
+--   | x == y = Right TrueVal
+--   | otherwise = Right FalseVal
+-- operate Less (IntVal x) (IntVal y)   
+--   | x < y = Right TrueVal
+--   | otherwise = Right FalseVal
+-- operate Greater (IntVal x) (IntVal y)    
+--   | x > y = Right TrueVal
+--   | otherwise = Right FalseVal
+-- -- Many more cases here. Haskell doesn't allow elem 1 ["hej"], but python does
+-- operate In (TrueVal) (ListVal l) 
+--   | elem TrueVal l = Right TrueVal
+--   | otherwise = Right FalseVal
 
 apply :: FName -> [Value] -> Comp Value
-apply f v 
+apply f v = undefined 
  --  | f == "print" = Comp (\e -> (Right v, [])) -- Stringify list of Value into monad output?
-  | f == "range" = case v of 
-    [IntVal x] -> let v = map (\x -> IntVal x) [0..1] 
-                  in Comp (\e -> (Right (ListVal v), []))
-    [IntVal x, IntVal y] -> let v = map (\x -> IntVal x) [x..y] 
-                            in Comp (\e -> (Right (ListVal v), []))
-    [IntVal x, IntVal y, IntVal z] -> let v = map (\x -> IntVal x) [z - 1, x..y]
-                                      in Comp (\e -> (Right (ListVal v), [])) -- [0, 1..3] -> [1,2,3]. z is 0-indexed]
-  | otherwise = undefined
+-- | f == "range" = case v of 
+--   [IntVal x] -> let v = map (\x -> IntVal x) [0..1] 
+--                 in Comp (\e -> (Right (ListVal v), []))
+--   [IntVal x, IntVal y] -> let v = map (\x -> IntVal x) [x..y] 
+--                           in Comp (\e -> (Right (ListVal v), []))
+--   [IntVal x, IntVal y, IntVal z] -> let v = map (\x -> IntVal x) [z - 1, x..y]
+--                                     in Comp (\e -> (Right (ListVal v), [])) -- [0, 1..3] -> [1,2,3]. z is 0-indexed]
+-- | otherwise = undefined
+  
 
 -- Main functions of interpreter
 -- eval e is the computation that evaluates the expression e in the current environment and returns its value
 eval :: Exp -> Comp Value
-eval (Const v) = Comp (\e -> (Right v, [])) -- Const List [Const Intval 1] or Const (IntVal 2)
-eval (Var v) = Comp (\e -> case lookup v e of {Just a -> (Right a, []); Nothing -> (Left (EBadVar v), [])})
-eval (Oper o x y) = do
-  x' <- eval x
-  y' <- eval y
-  v <- operate o x' y'
-  return $ case v of
-    Left e -> Comp (\e -> (Left e, []))
-    Right v -> Comp (\e -> (Right v, []))
+eval _ = undefined 
+-- eval (Const v) = Comp (\e -> (Right v, [])) -- Const List [Const Intval 1] or Const (IntVal 2)
+-- eval (Var v) = Comp (\e -> case lookup v e of {Just a -> (Right a, []); Nothing -> (Left (EBadVar v), [])})
+-- eval (Oper o x y) = do
+--   x' <- eval x
+--   y' <- eval y
+--   v <- operate o x' y'
+--   return $ case v of
+--     Left e -> Comp (\e -> (Left e, []))
+--     Right v -> Comp (\e -> (Right v, []))
   
 
 -- eval Not Exp = undefined
@@ -123,8 +131,8 @@ eval (Oper o x y) = do
 -- eval List [Exp] = undefined
 -- eval Compr Exp [CClause] = undefined
 
--- Likewise, exec p is the computation arising from executing the program (or program fragment) p, with no nominal return value, but with any side effects in p still taking
--- place in the computation.
+-- Likewise, exec p is the computation arising from executing the program (or program fragment) p, with 
+-- no nominal return value, but with any side effects in p still taking place in the computation.
 exec :: Program -> Comp () -- Program = [Stmt]
 exec p = undefined
 
@@ -133,7 +141,12 @@ exec p = undefined
 -- no variable bindings. (For implementing execute (only), you are allowed to use the 
 -- runComp projection of the monad type.)
 execute :: Program -> ([String], Maybe RunError)
-execute p = undefined
---  (x, y) <- runComp exec []
+execute p = ([show p], Nothing )
+
+  -- do (_, _) <- runComp (exec p) []
+  --    return (y, x)
+
+
+
 
           
