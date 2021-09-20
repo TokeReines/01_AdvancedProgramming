0aec9e0fc9e85245d6aea0a820c7d7f27eead9dc
Julian Wulff <julianwulff@gmail.com>
2021-09-20 18:56:44 +0200

    Added simple test for operate

--- Assignments/02/code/part2/tests/Test.hs
+++ Assignments/02/code/part2/tests/Test.hs
@@ -11,13 +11,31 @@ main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
 tests :: TestTree
-tests = testGroup "Stubby tests"
+tests = testGroup "Tests" [operateTest, stringifyValuesTest]
+
+operateTest = testGroup "operate tests" 
+  [ testCase "operate Plus (IntVal 2) (IntVal 2)" $ operate Plus (IntVal 2) (IntVal 2) @?= Right (IntVal 4)
+  , testCase "operate Plus (IntVal 2) (IntVal (-2))" $ operate Plus (IntVal 2) (IntVal (-2)) @?= Right (IntVal 0)
+  , testCase "operate Plus (IntVal (-2)) (IntVal (-2))" $ operate Plus (IntVal (-2)) (IntVal (-2)) @?= Right (IntVal (-4))
+  , testCase "operate Plus (IntVal (-2)) (IntVal 2)" $ operate Plus (IntVal (-2)) (IntVal 2) @?= Right (IntVal 0)
+  , testCase "*operate Plus TrueVal (IntVal 2)" $ operate Plus TrueVal (IntVal 2) @?= Left "Only integers allowed for Plus Op"
+  , testCase "*operate Plus FalseVal (IntVal 2)" $ operate Plus FalseVal (IntVal 2) @?= Left "Only integers allowed for Plus Op"
+  , testCase "*operate Plus (StringVal \"Hello\") (IntVal 2)" $ operate Plus (StringVal "Hello") (IntVal 2) @?= Left "Only integers allowed for Plus Op"
+  ]
+
+stringifyValuesTest = testGroup "stringifyValues Tests" 
+  [ testCase "stringifyValues [TrueVal, TrueVal, TrueVal]" $ stringifyValues [TrueVal, TrueVal, TrueVal] @?= "True True True"
+  , testCase "stringifyValues [IntVal 42, StringVal \"foo\", ListVal [TrueVal, ListVal []], IntVal (-1)]" $ stringifyValues [IntVal 42, StringVal "foo", ListVal [TrueVal, ListVal []], IntVal (-1)] @?= "42 foo [True, []] -1"
+
+  ]
+
+stubbyTest = testGroup "Stubby tests" 
   [testCase "crash test" $
     execute [SExp (Call "print" [Oper Plus (Const (IntVal 2))
                                            (Const (IntVal 2))]),
              SExp (Var "hello")]
-      @?= (["4"], Just (EBadVar "hello")),
-   testCase "execute misc.ast from handout" $
+      @?= (["4"], Just (EBadVar "hello"))
+  , testCase "execute misc.ast from handout" $
      do pgm <- read <$> readFile "examples/misc.ast"
         out <- readFile "examples/misc.out"
         execute pgm @?= (lines out, Nothing)]
