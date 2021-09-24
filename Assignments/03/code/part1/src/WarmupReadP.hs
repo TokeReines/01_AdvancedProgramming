8f25214e47fe45144784cc7a2eeb5c61d2a4712b
Julian Wulff <julianwulff@gmail.com>
2021-09-24 13:00:21 +0200

    updated grammar and added a test

--- Assignments/03/code/part1/src/WarmupReadP.hs
+++ Assignments/03/code/part1/src/WarmupReadP.hs
@@ -8,20 +8,9 @@ module WarmupReadP where
 --   tokens may be separated by arbtrary whitespace (spaces, tabs, newlines).
 
 -- Rewritten grammar, without left-recursion:
--- E ::= T E' | E' | T  
--- E' :== "+" T E' | "-" T E' | e
+-- E ::= T E' | "-" T E'
+-- E' :== "+" T E' | "-" T E' | E
 -- T ::= num | "(" E ")"
-
--- 2 + 2 = E(T(2), E'("+", T(num), E'(e)) )
--- 1+23-(-456) = E(T(1), E'("+", T(23), E'("-", T("(", E("-", T(456)), ")"), e))
--- -1+23-(-456) = E'("-", T(1), E'("+", T(23), E'("-", T("(", E(E'("-", T(456), E'(e))), ")"))))
-
-
--- 2 + 2 = E(T(2)), 
--- "-1 + 23 - (- 456)"
--- Add (Add (Negate (Num 1))
---          (Num 23))
---     (Negate (Negate (Num 456)))
 
 import Text.ParserCombinators.ReadP
 import Control.Applicative ((<|>))
@@ -42,12 +31,12 @@ parseString s =
     [(a,_)] -> Right a -- the_must be "", since 'eof' ok_-> error"oops, my grammar is ambiguous!
     _ -> error "You've made an oopsie"
 
--- E ::= T E' | E' | T  
+-- E ::= T E' | "-" T E'
 pE :: Parser Exp
 pE = do e <- pT; pE' e;
      <|> do symbol '-'; e <- pT; pE' (Negate e);
 
--- E' :== "+" T E' | "-" T E' | e
+-- E' :== "+" T E' | "-" T E' | E
 pE' :: Exp -> Parser Exp
 pE' e1 =  do ao <- pAddOp; e2 <- pT; pE' (ao e1 e2)
           <|>
@@ -79,39 +68,15 @@ whitespace = do munch isSpace; return ()
 lexeme :: Parser a -> Parser a
 lexeme p = do a <- p; whitespace; return a
 
--- E ::= T E' | E' | T  
--- pE :: Parser Exp
--- pE = 
---     do t <- pT; e' <- pE'; 
---     <|> 
---     do e' <- pE';
---     <|>
---     do t <- pT;
-
--- -- E' :== "+" T E' | "-" T E' | e
--- pE' :: Parser Exp
--- pE' = 
---     do ao <- pAdd; t <- pT; e' <- pE'; return $ Add t e' 
---     <|>
---     do satisfy isMinus; t <- pT; e' <- pE'; return $ Negate (Add t e')
---     <|>
---     do return Num 0
--- -- T ::= num | "(" E ")"
--- pT :: Parser Exp
--- pT = 
---     do n <- pNum; return $ Num n 
---     <|>
---     do symbol '('; e <- pE; symbol ')'; return e
-
--- pAdd :: Parser (Exp -> Exp -> Exp)
--- pAdd = 
-
--- symbol 
--- symbol c = \s -> satisfy s == c
-
--- isPlus :: Char -> Bool
--- isPlus c = c == '+'
-
--- isMinus :: Char -> Bool
--- isMinus c = c == '-'
-
+resultOfString = Right (Add (Add (Negate (Num 1)) (Num 23)) (Negate (Negate (Num 456))))
+testParseString =  parseString "-1+23-(-456)" == resultOfString
+                && parseString "-1" == Right (Negate (Num 1))
+                && parseString "1" == Right (Num 1)
+                && parseString "1+2" == Right (Add (Num 1 ) (Num 2))
+                && parseString "-1+2" == Right (Add (Negate (Num 1 )) (Num 2))
+                && parseString "1-2" == Right (Add (Num 1) (Negate (Num 2)))
+                && parseString "1-(-(-(-1)))" == Right (Add (Num 1) (Negate (Negate (Negate (Negate (Num 1))))))
+                && parseString " - 1 + 23 - ( - 456 ) " == resultOfString
+                && parseString "   -   1   +   23   -   (   -   456   ) " == resultOfString
+                && parseString "  \
+                \-   1\t   +\n   23   -   (   -   456   ) " == resultOfString
