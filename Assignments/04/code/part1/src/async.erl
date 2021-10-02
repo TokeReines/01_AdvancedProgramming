37d2e91af5bcd38528d41dfb346206a11894e5cc
Julian Wulff <julianwulff@gmail.com>
2021-10-02 16:51:48 +0200

    start of the solution

--- Assignments/04/code/part1/src/async.erl
+++ Assignments/04/code/part1/src/async.erl
@@ -2,7 +2,44 @@
 
 -export([new/2, wait/1, poll/1]).
 
-new(Fun, Arg) -> nope.
-wait(Aid) -> nope.
-poll(Aid) -> nope.
+% Aid stands for an actions ID, which is an opaque datatype (that is you decide what it should it should be)
+% Actionid -> 0.
+
+% request_reply(Pid, Request) ->
+%     Pid ! {self(), Request},
+%     receive
+%         {Pid, Response} -> Response
+%     end.
+
+% Færdig/result, 
+
+% starts a concurrent computatation that computes 'Fun(Arg)'. It returns an action ID (Aid)
+% A1 = async:new(fun ({X, Y}) -> X+Y end, {1,2}).
+% A2 = async:new(fun ({X}) -> timer:sleep(15000) end, 15000).
+-spec new(fun(), any()) -> pid().
+new(Fun, Arg) -> 
+      spawn(fun() -> 
+        Res = Fun(Arg),
+        receive
+          From -> From ! Res
+        end
+      end).
+    
+
+% Waits for an asyncronous action to complete, and return the values of the computation. If the asynchronous
+% action threw an exception, then the exceptions is rethrown by wait.
+wait(Aid) -> 
+  Aid ! self(),
+  receive
+    Res -> Res
+  end.
+
+
+% that check wether an asynchronoys action has completed yet. If it has not completed yet
+% then the result is 'nothing'; otherwise the result is '{exceptions, Ex}' if the 
+% the asynchronous action raised the exceptio 'Ex', or '{ok, Res}' if it returned the value 'Res'.
+poll(Aid) -> not_implemented.
+  % receive
+  %            {'nothing'} -> {'nothing'};
+  %        end.
 
--- Assignments/04/code/part2/src/emoji.erl
+++ Assignments/04/code/part2/src/emoji.erl
@@ -2,14 +2,37 @@
 
 -export([start/1, new_shortcode/3, alias/3, delete/2, lookup/2,
          analytics/5, get_analytics/2, remove_analytics/3,
-         stop/1]).
+         stop/1, isuniqueemojimap/1]).
 
 -type shortcode() :: string().
 -type emoji() :: binary().
--type analytic_fun(State) :: fun((shortcode(), State) -> State).
+% -type analytic_fun(State) :: fun((shortcode(), State) -> State).
+-type emojimap() :: [{shortcode(), emoji()}].
 
+-spec start(emojimap()) -> {any(), any()}.
+start(Initial) -> 
+  case isuniqueemojimap(Initial) of
+    true ->
+      E = spawn(fun () -> loop(Initial) end),
+      {ok, E};
+    false -> 
+      {error, "Initial emoji list contains duplicate of the same shortcode"}
+  end.
 
-start(_) -> not_implemented.
+-spec loop(emojimap()) -> any().
+loop(State) ->
+  receive
+    {From, {new_shortcode, Short, Emo}} -> 
+      {NewState, Res} = {{Short, Emo} ++ State, ok},
+      From ! {self(), Res},
+      loop(NewState)
+  end.
+
+request_reply(Pid, Request) ->
+  Pid ! {self(), Request},
+  receive
+    {Pid, Response} -> Response
+  end.
 
 new_shortcode(_, _, _) -> not_implemented.
 
@@ -26,3 +49,17 @@ get_analytics(_, _) -> not_implemented.
 remove_analytics(_, _, _) -> not_implemented.
 
 stop(_) -> not_implemented.
+
+% Helper Functions
+-spec isuniqueemojimap(emojimap()) -> boolean().
+isuniqueemojimap(EmojiMap) -> 
+  if 
+    EmojiMap == [] -> true;
+    true -> UniqueEmojiMap = lists:ukeysort(1, EmojiMap),
+            length(EmojiMap) == length(UniqueEmojiMap)
+  end.
+
+% Shortcuts for the terminal
+% c("emoji.erl").
+% Emojis = [{"algeria", <<"🇩‍🇿"/utf8>>},{"afghanistan", <<"🇦‍🇫"/utf8>>},{"abcd", <<"🔡"/utf8>>}].
+% Emojis2 = [{"algeria", <<"🇩‍🇿"/utf8>>},{"afghanistan", <<"🇦‍🇫"/utf8>>},{"abcd", <<"🔡"/utf8>>}, {"algeria", <<"🇩‍🇿"/utf8>>}].
