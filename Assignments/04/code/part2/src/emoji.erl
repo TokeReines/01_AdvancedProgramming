7d692401746d4ae754388efe963d15df04ab784a
Julian Wulff <julianwulff@gmail.com>
2021-10-03 17:56:49 +0200

    Basic solution for the server

--- Assignments/04/code/part2/src/emoji.erl
+++ Assignments/04/code/part2/src/emoji.erl
@@ -22,25 +22,53 @@ start(Initial) ->
 -spec loop(emojimap()) -> any().
 loop(State) ->
   receive
-    {From, {new_shortcode, Short, Emo}} -> 
-      {NewState, Res} = {{Short, Emo} ++ State, ok},
-      From ! {self(), Res},
-      loop(NewState)
+    % * New Shortcode
+    {From, {new_shortcode, Short, Emo}} ->
+      case isnewshortcode(Short, State) of % ! should maybe be performed in its own process
+        true -> 
+          {NewState, Res} = {State ++ [{Short, Emo}], ok},
+          From ! {self(), Res},
+          loop(NewState);
+        false ->
+          From ! {self(), {error, "Shortcode already exists"}}
+      end;
+    {delete, Short} -> 
+      {NewState} = lists:keydelete(Short, 1, State),
+      loop(NewState);
+    % ! Stop server - Should also stop all other process'.
+    {From, stop} -> From ! {self(), ok};
+    % ! Look up - Should evaluate analytics functions when looked up
+    {From, {lookup, Short}} ->
+      Retval = lists:keysearch(Short, 1, State), % ! should maybe be performed in its own process
+      case Retval of
+        {value, {_Short, Emoji}} -> 
+          Res = {ok, Emoji},
+          From ! {self(), Res},
+          loop(State);
+        false -> 
+          From ! {self(), no_emoji},
+          loop(State)
+      end
   end.
 
+-spec request_reply(pid(), any()) -> any().
 request_reply(Pid, Request) ->
   Pid ! {self(), Request},
   receive
     {Pid, Response} -> Response
   end.
 
-new_shortcode(_, _, _) -> not_implemented.
+
+
+-spec new_shortcode(pid(), shortcode(), emoji()) -> any().
+new_shortcode(E, Short, Emo) -> request_reply(E, {new_shortcode, Short, Emo}).
 
 alias(_, _, _) -> not_implemented.
 
 delete(_, _) -> not_implemented.
 
-lookup(_, _) -> not_implemented.
+-spec lookup(pid(), shortcode()) -> any().
+lookup(E, Short) -> request_reply(E, {lookup, Short}).
 
 analytics(_, _, _, _, _) -> not_implemented.
 
@@ -48,7 +76,8 @@ get_analytics(_, _) -> not_implemented.
 
 remove_analytics(_, _, _) -> not_implemented.
 
-stop(_) -> not_implemented.
+-spec stop(pid()) -> any().
+stop(E) -> request_reply(E, stop).
 
 % Helper Functions
 -spec isuniqueemojimap(emojimap()) -> boolean().
@@ -59,7 +88,13 @@ isuniqueemojimap(EmojiMap) ->
             length(EmojiMap) == length(UniqueEmojiMap)
   end.
 
+isnewshortcode(Short, EmojiList) ->
+  not lists:keymember(Short, 1, EmojiList).
+
 % Shortcuts for the terminal
 % c("emoji.erl").
 % Emojis = [{"algeria", <<"🇩‍🇿"/utf8>>},{"afghanistan", <<"🇦‍🇫"/utf8>>},{"abcd", <<"🔡"/utf8>>}].
 % Emojis2 = [{"algeria", <<"🇩‍🇿"/utf8>>},{"afghanistan", <<"🇦‍🇫"/utf8>>},{"abcd", <<"🔡"/utf8>>}, {"algeria", <<"🇩‍🇿"/utf8>>}].
+% {ok, E} = emoji:start(Emojis).
+% emoji:lookup(E, "algeria").
+% emoji:new_shortcode(E, "bat", <<"🦇"/utf8>>).
--- Assignments/04/code/part2/tests/test_emoji.erl
+++ Assignments/04/code/part2/tests/test_emoji.erl
@@ -10,7 +10,10 @@ test_all() -> eunit:test(testsuite(), [verbose]).
 testsuite() ->
     [ {"Basic behaviour", spawn,
        [ test_start_server()
+       , test_start_server_with_emoji()
+       , test_start_server_with_duplicate_shortcode()
        , test_shortcode_smiley()
+       , test_duplicate_shortcode_smiley()
        ]
       }
     ].
@@ -21,6 +24,18 @@ test_start_server() ->
        ?assertMatch({ok, _}, emoji:start([]))
      end }.
 
+test_start_server_with_emoji() -> 
+    {"We can call start/1 with some emojis and it does not crash",
+      fun () ->
+        ?assertMatch({ok, _}, emoji:start(someemoji:small()))
+      end }.
+
+test_start_server_with_duplicate_shortcode() -> 
+    {"Calling start/1 with emojis with non-unique shortcodes returns an error",
+      fun () ->
+        ?assertMatch({error, _}, emoji:start([{"+1", <<"👍️"/utf8>>}, {"+1", <<"👍️"/utf8>>}]))
+      end }.
+
 test_shortcode_smiley() ->
     {"Register new shortcode",
      fun () ->
@@ -28,3 +43,16 @@ test_shortcode_smiley() ->
        ?assertEqual(ok, emoji:new_shortcode(S, "smiley",
                                             <<240,159,152,131>>))
      end }.
+
+test_duplicate_shortcode_smiley() ->
+    {"Registering existing shortcode returns error",
+     fun () ->
+       {ok, S} = emoji:start([{"smiley", <<240,159,152,131>>}]),
+       ?assertMatch({error, _}, emoji:new_shortcode(S, "smiley", <<240,159,152,131>>))
+     end }.
+
+
+% c("../src/emoji.erl").
+% c(test_emoji).
+% c(someemoji).
+% test_emoji:test_all().
