427e25be9af34e826922bcfce7eff42725fdb9a4
Toke Reines <toke.reines@gmail.com>
2021-10-06 09:28:55 +0200

    Stuff on async.erl

--- Assignments/04/code/part1/src/async.erl
+++ Assignments/04/code/part1/src/async.erl
@@ -1,6 +1,6 @@
 -module(async).
 
--export([loop/1 ,new/2, wait/1, poll/1, setup/0]).
+-export([loop/1 ,new/2, wait/1, poll/1, funfun/0]).
 
 
 loop(State) ->
@@ -10,62 +10,51 @@ loop(State) ->
             io:fwrite("New~n"),
             Me = self(),
             process_flag(trap_exit, true),
-            spawn_link (fun() ->
+            NewWorker = spawn_link (fun() ->
                 Res = Fun(Arg),
                 Me ! {finished, Res},
                 receive
                     From -> From ! Res
                 end
             end),
-            loop(State);
-        {finished, Result} -> 
-            io:fwrite("Fun finished~n"),
-            loop({Worker, Result});
+            loop({NewWorker, Result});
+        {finished, Res} -> 
+            io:fwrite("Finished~n"),
+            loop({Worker, Res});
         {wait, From} -> 
-            io:fwrite("Waiting~n"),
+            io:fwrite("Wait~n"),
             case Result of
+                nothing ->  Worker ! From;
                 {exception, Reason} ->  From ! {exception, Reason};
                 _ -> Worker ! From
             end,            
             loop(State);
         {poll, From} -> 
-            io:fwrite("Polling~n"),
+            io:fwrite("Poll~n"),
             case Result of
-                nothing -> io:fwrite("nothing~n"), From ! nothing;
-                {exception, Reason} -> io:fwrite("exception~n"), From ! {exception, Reason};
-                true -> io:fwrite("basecase~n"), From ! {ok, Result}
+                nothing -> From ! nothing;
+                {exception, Reason} -> From ! {exception, Reason};
+                _ -> From ! {ok, Result}
             end,
             loop(State);
         {'EXIT', Worker, Reason} -> 
-            io:fwrite("Process died~n"),
+            io:fwrite("Exit~n"),
             loop({Worker, {exception, Reason}})
     end.
-    
 
-%new(Fun, Arg) that starts a concurrent computation that computes Fun(Arg). It returns an action ID
-%X = async:new(fun({}) -> timer:sleep(15000) end, {}).
 new(Fun, Arg) -> 
     Worker = spawn(fun() -> loop({nothing, nothing}) end),
     Worker ! {new, {Fun, Arg}},
     Worker.
 
-% Waits for an asyncronous action to complete, and return the values of the computation. If the asynchronous
-% action threw an exception, then the exceptions is rethrown by wait.
 wait(Aid) -> 
     Aid ! {wait, self()},
     receive
         Res -> Res
     end.
 
-% that check wether an asynchronoys action has completed yet. If it has not completed yet
-% then the result is 'nothing'; otherwise the result is '{exceptions, Ex}' if the 
-% the asynchronous action raised the exceptio 'Ex', or '{ok, Res}' if it returned the value 'Res'.
 poll(Aid) -> 
     Aid ! {poll, self()},
     receive
         Res -> Res
     end.
-
-setup() ->
-    E4 = async:new(fun(N) ->N+1 end, 1),
-    async:poll(E4).
