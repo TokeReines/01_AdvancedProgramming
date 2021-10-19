704319248b993055efff9f96de052b72f81505ee
Toke Reines <toke.reines@gmail.com>
2021-10-19 16:50:43 +0200

    werk

--- Assignments/06/code/part1/src/async.erl
+++ Assignments/06/code/part1/src/async.erl
@@ -2,8 +2,13 @@
 
 -export([new/2, wait/1, poll/1, wait_catch/1, wait_any/1]).
 
-new(Fun, Arg) -> nope.
-wait(Aid) -> nope.
-poll(Aid) -> nope.
+% New: Supervisor()  -> Worker
+% Wait: Supervisor() -> await NewSupervisor
+% Poll: poll NewSupervisor
+
+% Starts a supvisor with a worker that invokes the function
+new(Fun, Arg) -> nope. 
+wait(Aid) -> nope. % Spawns a supervisor that waits for ^ supervisor to complete
+poll(Aid) -> nope. % Polls the supervisor from new()
 wait_catch(Aid) -> nope.
 wait_any(Aids) -> nope.
