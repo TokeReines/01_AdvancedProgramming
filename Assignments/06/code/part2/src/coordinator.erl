aa77fa6294b207415e1eee181ba3902b77292069
Toke Reines <toke.reines@gmail.com>
2021-10-21 22:57:25 +0200

    coordinator werk

--- Assignments/06/code/part2/src/coordinator.erl
+++ Assignments/06/code/part2/src/coordinator.erl
@@ -4,7 +4,7 @@
 
 -export([start/3, move/2]).
 -export([callback_mode/0, init/1]).
--export([idle/3, rock/3, paper/3, scissor/3]).
+-export([idle/3, rock/3, paper/3, scissor/3, invalid_choice/3]).
 -export([test/0]).
 
 %%% -------------------------------------------------------
@@ -34,9 +34,7 @@ move(Coordinator, Choice) ->
 init({Player1, Player2, N}) ->
     State = idle,
     Data = 
-        #{player1 => {Player1, []},
-          player2 => {Player2, []},
-          bestOf => N,
+        #{bestOf => N,
           nonties => 0,
           ties => 0},
     {ok, State, Data}.
@@ -50,6 +48,10 @@ callback_mode() ->
 
 idle({call, From}, Choice, Data) ->
     io:format("You chose ~w! Waiting for opponent?. ~n", [Choice]),
+    % case lists:member(Choice, [rock, paper, scissor]) of
+    %     true -> {next_state, Choice, {From, Data}};
+    %     false -> {next_state, invalid_choice, {From, Data}}
+    % end.
     {next_state, Choice, {From, Data}}.
 
 rock({call, Player2}, Choice, {Player1, Data}) ->
@@ -57,7 +59,8 @@ rock({call, Player2}, Choice, {Player1, Data}) ->
     case Choice of
         rock -> tie(Data, Player1, Player2);
         paper -> nontie(Data, Player2, Player1, paper);
-        scissor -> nontie(Data, Player1, Player2, rock)
+        scissor -> nontie(Data, Player1, Player2, rock);
+        _ -> nontie(Data, Player1, Player2, scissor)
     end.
 
 paper({call, Player2}, Choice, {Player1, Data}) ->
@@ -65,7 +68,8 @@ paper({call, Player2}, Choice, {Player1, Data}) ->
     case Choice of
         rock -> nontie(Data, Player1, Player2, paper);
         paper -> tie(Data, Player1, Player2);
-        scissor -> nontie(Data, Player2, Player1, scissor)
+        scissor -> nontie(Data, Player2, Player1, scissor);
+        _ -> nontie(Data, Player1, Player2, paper)
     end.
 
 scissor({call, Player2}, Choice,  {Player1, Data}) ->
@@ -73,7 +77,17 @@ scissor({call, Player2}, Choice,  {Player1, Data}) ->
     case Choice of
         rock -> nontie(Data, Player2, Player1, rock);
         paper -> nontie(Data, Player1, Player2, scissor);
-        scissor -> tie(Data, Player1, Player2)
+        scissor -> tie(Data, Player1, Player2);
+        _ -> nontie(Data, Player1, Player2, scissor)
+    end.
+
+invalid_choice({call, Player2}, Choice, {Player1, Data}) ->
+    io:format("Invalid choice: You made a ~w move!  ~n", [Choice]),
+    case Choice of
+        rock -> nontie(Data, Player2, Player1, Choice);
+        paper -> nontie(Data, Player2, Player1, Choice);
+        scissor -> nontie(Data, Player2, Player1, Choice);
+        _ -> tie(Data, Player1, Player2) % Both made invalid moves
     end.
 
 %%% -------------------------------------------------------
@@ -81,14 +95,19 @@ scissor({call, Player2}, Choice,  {Player1, Data}) ->
 %%% -------------------------------------------------------
 
 tie(Data, Player1, Player2) ->
+    io:format("Tie ~n"),
     #{ties := Ties} = Data,
-    NewData = Data#{ties := Ties + 1},
+    NewData = Data#{ ties := Ties + 1 },
     {next_state, idle, NewData, [{reply, Player2, tie}, {reply, Player1, tie}]}.
 
 nontie(Data, Winner, Loser, WinningMove) ->
-    #{nonties := NonTies} = Data,
-    NewData = Data#{nonties := NonTies + 1},
-    {next_state, idle, NewData, [{reply, Winner, win}, {reply, Loser, {loss, WinningMove}}]}.
+    io:format("Nontie ~n"),
+    #{ nonties := NonTies, bestOf := BestOf} = Data, % Winner := WinCount 
+    NewData = Data#{ nonties := NonTies + 1 }, % Winner := WinCount + 1. TODO: Winner is a PID, which we dont have at time of creation of coordinator.
+    case NonTies + 1 == BestOf of
+        true -> {next_state, idle, NewData, [{reply, Winner, {game_over, 0, 0}}, {reply, Loser, {game_over, 0, 0}}]}; 
+        false -> {next_state, idle, NewData, [{reply, Winner, win}, {reply, Loser, {loss, WinningMove}}]}
+    end.
 
 %%% -------------------------------------------------------
 %%% Appendix
@@ -97,10 +116,18 @@ nontie(Data, Winner, Loser, WinningMove) ->
 test() ->
     {ok, Coordinator} = start("P1", "P2", 3),
     spawn(fun() ->
-             Res = move(Coordinator, scissor),
-             io:format("P1: ~w ~n", [Res])
+             Res1 = move(Coordinator, paper),
+             io:format("P1: ~w ~n", [Res1]),
+             Res2 = move(Coordinator, paper),
+             io:format("P1: ~w ~n", [Res2]),
+             Res3 = move(Coordinator, paper),
+             io:format("P1: ~w ~n", [Res3])
           end),
     spawn(fun() ->
-             Res = move(Coordinator, paper),
-             io:format("P2: ~w ~n", [Res])
+             Res1 = move(Coordinator, rock),
+             io:format("P2: ~w ~n", [Res1]),
+             Res2 = move(Coordinator, scissor),
+             io:format("P2: ~w ~n", [Res2]),
+             Res3 = move(Coordinator, rock),
+             io:format("P2: ~w ~n", [Res3])
           end).
