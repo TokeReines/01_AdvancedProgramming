4c5d179a35a8e0c54b7d8cc177677a5adbcbcff3
Julian Wulff <julianwulff@gmail.com>
2021-10-26 16:20:22 +0200

    Boilerplate for proper testing

--- Assignments/06/code/part2/tests/eqc_rps.erl
+++ Assignments/06/code/part2/tests/eqc_rps.erl
@@ -1,54 +1,40 @@
 -module(eqc_rps).
+
 -include_lib("eqc/include/eqc.hrl").
 -include("apqc_statem.hrl").
 
 -behaviour(apqc_statem).
-
 -export([initial_state/0, command/1, precondition/2, postcondition/3, next_state/3]).
 
-%% Maybe you want to use eunit
--include_lib("eunit/include/eunit.hrl").
+-export([prop_rps/0]).
 
 
+prop_rps() -> 
+  ?FORALL(Cmds, commands(?MODULE), 
+  begin
+    {_, S, _} = Result = run_commands(?MODULE, Cmds),
+    cleanup(S),
+    check_commands(Cmds, Result)   
+  end).
 
-% AP QuickCheck State-Machine Library
-% ===================================
+check_commands(Cmds, {_, _, Res} = HSRes) ->
+  pretty_commands(?MODULE, Cmds, HSRes,
+                  aggregate(command_names(Cmds),
+                            equals(Res, ok))).
+  
 
-% This is a (partial) implementation of the `eqc_statem` library from
-% Quviq AB. It intended to have a subset of the API offered by
-% `eqc_statem`. Thus, any documentation for `eqc_statem` should be
-% usable as documentation for this library.
+cleanup(_S) -> 
+  ok.
 
-% The library is build on top of Erlang QuickCheck Mini from Quviq AB,
-% which you need to install first.
+initial_state() ->
+  #{}.
 
+command(_S) -> 
+  oneof([]).
 
-% Not for redistribution
-% ----------------------
+next_state(S, _, _) ->
+  S.
 
-% This library is only for use on the course Advanced Programming, and
-% should **not** be redistributed.
+precondition(_, _) -> true.
 
-
-% Getting started
-% ---------------
-
-% 1. Make sure you have Erlang QuickCheck Mini (or the full version)
-%    installed.
-
-% 2. Move the files `apqc_statem.erl` and `apqc_statem.hrl` to the
-%    directory with your callback module for used with `apqc_statem`.
-
-% 3. Add the following lines to your callback module:
-
-%     ~~~{.erlang}
-%     -include_lib("eqc/include/eqc.hrl").
-%     -include("apqc_statem.hrl").
-
-%     -behaviour(apqc_statem).
-%     -export([initial_state/0, command/1, precondition/2, postcondition/3, next_state/3]).
-%     ~~~
-
-% 4. Implement you callback module.
-
-% 5. Profit.
+postcondition(_, _, _) -> true.
