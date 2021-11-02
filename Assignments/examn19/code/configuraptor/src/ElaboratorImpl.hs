b45b6ff74165b162d4275105c9fe473125f93f5f
Toke Reines <toke.reines@gmail.com>
2021-11-02 12:08:58 +0100

    Added code for haskell only lacking SolverImpl

---
+++ Assignments/examn19/code/configuraptor/src/ElaboratorImpl.hs
@@ -0,0 +1,10 @@
+module ElaboratorImpl where
+
+import Absyn
+-- add other imports
+
+lookres :: [Resource] -> RName -> Either ErrMsg Resource
+lookres = undefined
+
+elaborate :: IDB -> Either ErrMsg DB
+elaborate = undefined
---
+++ Assignments/examn19/code/optimistic/src/optimistic.erl
@@ -0,0 +1,12 @@
+-module(optimistic).
+
+-export([start/1, stop/1, reset/2, delete/2, operation/3, commit/1, abort/1]).
+
+
+start(_) -> not_implemented.
+stop(_) -> not_implemented.
+reset(_,_) -> not_implemented.
+delete(_, _) -> not_implemented.
+operation(_,_,_) -> not_implemented.
+commit(_) -> not_implemented.
+abort(_) -> not_implemented.
