28dd219abf4268820bd6738fcd900185d27b54af
Toke Reines <toke.reines@gmail.com>
2021-11-07 19:33:25 +0100

    Added all but upsert and stable to erlang part

--- Assignments/toke_examn/code/frappe/src/frappe.erl
+++ Assignments/toke_examn/code/frappe/src/frappe.erl
@@ -1,5 +1,8 @@
 -module(frappe).
-
+-behaviour(gen_server).
+-import(item, []).
+-import(queue, []).
+-export([init/1, handle_call/3, handle_cast/2]).
 % You are allowed to split your Erlang code in as many files as you
 % find appropriate.
 % However, you MUST have a module (this file) called frappe.
@@ -16,34 +19,186 @@
          stop/1
         ]).
 
-% You may have other exports as well
--export([]).
+-type key() :: term().
+-type value() :: term().
+-type cost() :: pos_integer().
+-type transformation() :: fun(({existing, value()} | new) ->
+  {new_value, value(), cost()} | any()).
+
+%%% -------------------------------------------------------
+%%% API
+%%% -------------------------------------------------------
+
+% Jeg tænker en Frappe gen_server, og en gen_statem til hvert item. Så kan man holde track på om et item er ved at blive "write"d i. Frappe skal så have holde styr på sin capacity, en queue for LRU, og et dict der mapper keys til processes
+% Starts a new Frappe server with capacity Cap > 0
+-spec fresh(pos_integer()) -> term().
+fresh(Cap) ->
+  gen_server:start(?MODULE, Cap, []).
+
+-spec set(pid(), key(), value(), cost()) -> term().
+set(FS, Key, Value, Cost) ->
+  gen_server:call(FS, {set, Key, Value, Cost}).
+
+-spec read(pid(), key()) -> term().
+read(FS, Key) ->
+  gen_server:call(FS, {read, Key}).
+
+-spec insert(pid(), key(), value(), cost()) -> term().
+insert(FS, Key, Value, Cost) ->
+  gen_server:call(FS, {insert, Key, Value, Cost}).
+
+-spec update(pid(), key(), value(), cost()) -> term().
+update(FS, Key, Value, Cost) ->
+  gen_server:call(FS, {update, Key, Value, Cost}).
+
+-spec upsert(pid(), key(), transformation()) -> any().
+upsert(FS, Key, Fun) ->
+  gen_server:call(FS, {upsert, Key, Fun}).
+
+-spec stable(pid(), key(), any()) -> term().
+stable(FS, Key, Ref) ->
+  gen_server:cast(FS, {stable, Key, Ref}).
+
+-spec all_items(pid()) -> term().
+all_items(FS) ->
+  gen_server:call(FS, all_items).
+
+-spec stop(pid()) -> term().
+stop(FS) ->
+  % Does this just kill it or call the server to manually cleanup?
+  gen_server:stop(FS).
 
 
 
-fresh(_) ->
-  not_implemented.
+%%% -------------------------------------------------------
+%%% Callback Functions
+%%% -------------------------------------------------------
 
-set(_, _, _, _) ->
-  not_implemented.
+init(Cap) ->
+  if Cap < 1 ->
+    {error, "Cap is not positive"};
+    true -> 
+      State = #{
+        cap => Cap,
+        lru => queue:new(), 
+        items => #{}
+      },
+      {ok, State}
+  end.
 
-read(_, _) ->
-  not_implemented.
+%%% -------------------- Read Item -------------------------
+handle_call({read, Key}, _From, State) -> 
+  #{items := Items, lru := LRU} = State,
+  case maps:get(Key, Items, false) of 
+    {Value, _Cost} -> 
+      NewLRU = update_lru(Key, LRU),
+      {reply, {ok, Value}, State#{lru := NewLRU }};
+    false -> {reply, nothing, State}
+  end;
 
-insert(_, _, _, _) ->
-  not_implemented.
+%%% -------------------- All Items -------------------------
+handle_call(all_items, _From, State) -> 
+  #{items := Items} = State,
+  AllItems = maps:fold(fun(Key, {Value, Cost}, List) -> List ++ [{Key, Value, Cost}] end, [], Items),
+  {reply, AllItems, State};
 
-update(_, _, _, _) ->
-  not_implemented.
+%%% -------------------- Set Item -------------------------
+handle_call({set, Key, Value, Cost}, _From, State) -> 
+  #{cap := Cap, 
+    lru := LRU,
+    items := Items } = State,
+  if Cost > Cap ->
+    {reply, {error, "Cap exceeded"}, State};
+    true -> 
+      {NLRU, NItems} = set_item(LRU, Items, Key, Value, Cost, Cap),
+      {reply, ok, State#{lru := NLRU, items := NItems}}
+  end;
 
-upsert(_, _, _) ->
-  not_implemented.
+%%% -------------------- Insert Item -------------------------
+handle_call({insert, Key, Value, Cost}, _From, State) -> 
+  #{ cap := Cap, lru := LRU, items := Items } = State,
+  if Cost > Cap ->
+      {reply, {error, "Cap exceeded"}, State};
+    true -> 
+      case maps:get(Key, Items, false) of 
+        {Value, Cost} -> 
+          {reply, {error, "Duplicate item"}, State};
+        false -> 
+          {NLRU, NItems} = set_item(LRU, Items, Key, Value, Cost, Cap),
+          {reply, ok, State#{lru := NLRU, items := NItems}}     
+      end
+  end;
 
-stable(_, _, _) ->
-  not_implemented.
+%%% -------------------- Update Item -------------------------
+handle_call({update, Key, Value, Cost}, _From, State) -> 
+  #{ cap := Cap, lru := LRU, items := Items } = State,
+  if Cost > Cap ->
+      {reply, {error, "Cap exceeded"}, State};
+    true -> 
+      case maps:get(Key, Items, false) of 
+        {Value, Cost} -> 
+          {NLRU, NItems} = set_item(LRU, Items, Key, Value, Cost, Cap),
+          {reply, ok, State#{lru := NLRU, items := NItems}};
+        false ->                
+          {reply, {error, "Item not found"}, State}
+      end
+  end;
 
-all_items(_) ->
-  not_implemented.
+%%% -------------------- Upsert Item -------------------------
+handle_call({upsert, Key, Value, Cost}, _From, State) -> 
+  #{ cap := Cap, lru := LRU, items := Items } = State,
+  if Cost > Cap ->
+      {reply, {error, "Cap exceeded"}, State};
+    true -> 
+      case maps:get(Key, Items, false) of 
+        {Value, Cost} -> 
+          {NLRU, NItems} = set_item(LRU, Items, Key, Value, Cost, Cap),
+          {reply, {ok}, State#{lru := NLRU, items := NItems}};
+        false ->                
+          {reply, {error, "Item not found"}, State}
+      end
+  end.
 
-stop(_) ->
-  not_implemented.
+handle_cast(_Request, _State) -> not_implemented.
+
+%%% -------------------------------------------------------
+%%% Auxiliary Functions
+%%% -------------------------------------------------------
+
+set_item(LRU, Items, Key, Value, Cost, Cap) -> 
+  {NLRU, RItems} = make_room(LRU, Items, Key, Cost, Cap),
+  CItems = clean_items(Items, RItems),
+  ULRU = update_lru(Key, NLRU),
+  NewItems = CItems#{Key => {Value, Cost}},
+  {ULRU, NewItems}.
+
+update_lru(Key, Queue) ->
+  UQueue = queue:delete(Key, Queue),
+  queue:in(Key, UQueue).
+
+get_load(Items) ->
+  maps:fold(fun(_K, {_V, Cost}, Sum) -> Cost + Sum end, 0, Items).
+
+% Remove popped items in our Items map to keep them synchronized
+clean_items(Items, RItems) ->
+  maps:without(RItems, Items).
+
+make_room(Queue, Items, Key, Cost, Cap) -> 
+  NQueue = queue:delete(Key, Queue),
+  % Avoid duplicates by removing it
+  Load = get_load(Items),
+  % Make room in the queue for the new Cost
+  pop_to_cap(NQueue, Items, Cost, Cap, Load, []).
+
+% Keep popping until we have Capacity for the new Cost
+pop_to_cap(Queue, Items, Cost, Cap, Load, RItems) ->
+  if Load + Cost =< Cap ->    
+      {Queue, RItems};
+    true ->
+      case queue:out(Queue) of
+        {{value, Key}, NewQueue} -> 
+          {_Value, ICost} = maps:get(Key, Items),
+          pop_to_cap(NewQueue, Items, Cost, Cap, Load - ICost, RItems ++ [Key]);
+        {empty, NewQueue} -> {NewQueue, RItems}
+      end
+  end.
--- Assignments/toke_examn/code/frappe/src/test_frappe.erl
+++ Assignments/toke_examn/code/frappe/src/test_frappe.erl
@@ -3,14 +3,144 @@
 -export([test_all/0, test_everything/0]).
 -export([]). % Remember to export the other functions from Q2.2
 
+%% Maybe you want to use eunit
+-include_lib("eqc/include/eqc.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 % You are allowed to split your testing code in as many files as you
 % think is appropriate, just remember that they should all start with
 % 'test_'.
 % But you MUST have a module (this file) called test_frappe.
 
-test_all() ->
-  ok.
+test_all() -> eunit:test(testsuite(), [verbose]).
+
+
+testsuite() -> 
+      [ {"Basic behavior",  spawn,
+          [ test_fresh(),
+            test_set(),
+            test_read(),
+            test_read_lru(),
+            test_set_break_cap(),
+            test_insert(),
+            test_insert_break_cap(),
+            test_insert_duplicate(),
+            test_insert_duplicate_break_cap(),
+            test_all_items(),
+            test_all_items_break_cap()
+          ]
+        }
+      ].
+
+test_fresh() ->
+    {"Start a frappe, and nothing else",
+     fun() ->
+             ?assertMatch({ok, _}, frappe:fresh(5))
+     end}.
+
+test_set() -> 
+    {"Test set, legal",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:set(A, "a", "a", 3)),
+      frappe:stop(A)
+    end
+    }.
+
+test_read() -> 
+    {"Test read, simple",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:set(A, "a", "value", 3)),
+      ?assertMatch({ok, "value"}, frappe:read(A, "a")),
+      frappe:stop(A)
+    end
+    }.
+
+test_read_lru() -> 
+    {"Test read, lru update after",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 3)),
+      ?assertMatch(ok, frappe:insert(A, "b", "b", 2)),
+      ?assertMatch({ok, "a"}, frappe:read(A, "a")),
+      ?assertMatch(ok, frappe:insert(A, "c", "c", 2)),
+      ?assertMatch({ok, "a"}, frappe:read(A, "a")),
+      ?assertMatch(nothing, frappe:read(A, "b")),
+      frappe:stop(A)
+    end
+    }.
+
+test_set_break_cap() -> 
+    {"Test Set breaking the Cap",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch({error, _}, frappe:set(A, "a", "a", 6)),
+      frappe:stop(A)
+    end
+    }.
+
+test_insert() -> 
+    {"Test insert",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 3)),
+      frappe:stop(A)
+    end
+    }.
+
+test_insert_break_cap() -> 
+    {"Test insert breaking cap",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch({error, _}, frappe:insert(A, "a", "a", 6)),
+      frappe:stop(A)
+    end
+    }.
+
+test_insert_duplicate() -> 
+    {"Test insert duplicate error",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 3)),
+      ?assertMatch({error, _}, frappe:insert(A, "a", "a", 3)),
+      frappe:stop(A)
+    end
+    }.
+
+test_insert_duplicate_break_cap() -> 
+    {"Test insert duplicate after cap break",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 3)),
+      ?assertMatch(ok, frappe:insert(A, "b", "a", 3)),
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 3)),
+      frappe:stop(A)
+    end
+    }.
+
+test_all_items() -> 
+    {"Test insert duplicate after cap break",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 1)),
+      ?assertMatch(ok, frappe:insert(A, "b", "a", 2)),
+      ?assertMatch([{"a", "a", 1}, {"b", "a", 2}], frappe:all_items(A)),
+      frappe:stop(A)
+    end
+    }.
+
+test_all_items_break_cap() -> 
+    {"Test insert duplicate after cap break",
+     fun() ->
+      {ok, A} = frappe:fresh(5),      
+      ?assertMatch(ok, frappe:insert(A, "a", "a", 1)),
+      ?assertMatch(ok, frappe:insert(A, "b", "a", 2)),
+      ?assertMatch(ok, frappe:insert(A, "bigone", "a", 5)),
+      ?assertMatch([{"bigone", "a", 5}], frappe:all_items(A)),
+      frappe:stop(A)
+    end
+    }.
 
 test_everything() ->
   test_all().
